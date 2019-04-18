0dace51ea2f6b580c8ee4e100015a3eb1a314695
abisummers <hello@abisummers.com>
2019-04-18 10:03:32 +0200

    code done as reinforcement lesson

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,80 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const User = require("../models/user-model")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+// ---------- GET NAMES ---------------
+router.get("/names", (req, res, next) => {
+  User.find()
+    .then(userDoc => {
+      console.log(userDoc);
+      res.render("names.hbs", { userDoc })
+    })
+    .catch(err => next(err))
+})
+
+// -------------- GET DETAILS -------------
+
+router.get("/details/:id", (req, res, next) => {
+  console.log(req.params.id)
+
+  User.find({ _id: { $eq: req.params.id } })
+    .then(userDoc => {
+      console.log(userDoc);
+      res.render("details.hbs", { userDoc })
+    })
+    .catch(err => next(err))
+})
+
+// -------------- CREATE USER -------------
+
+router.get("/add", (req, res, next) => {
+  res.render("add.hbs")
+})
+
+router.post("/process-add", (req, res, next) => {
+  const { firstName, description } = req.body
+  console.log(firstName, description);
+
+  User.create(
+    { firstName, description },
+  )
+    .then(userDoc => {
+      console.log(userDoc)
+      res.redirect("/names")
+    })
+    .catch(err => next(err))
+})
+
+// --------- UPDATE USER ----------
+
+router.get("/edit/:id", (req, res, next) => {
+  User.find({ _id: { $eq: req.params.id } })
+    .then(userDoc => {
+      res.render("edit.hbs", { userDoc })
+
+    }).catch(err => next(err))
+})
+
+router.post("/process-edit/:id", (req, res, next) => {
+  const { firstName, description } = req.body
+
+  User.findByIdAndUpdate(
+    req.params.id,
+    { firstName, description },
+    { runValidators: true }
+  )
+    .then(userDoc => {
+      console.log(userDoc);
+      res.redirect("/")
+    })
+    .catch(err => next(err))
+})
+
 
 module.exports = router;
